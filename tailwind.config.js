d1802e6b9f97ae8d6d8fbdb940be7c397d17a839
juanmesa2097 <juanmesa2097@gmail.com>
2020-06-30 18:50:35 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ["Rubik", "sans-serif"],
+      body: ["Rubik", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "soft-blue": "#5368df",
+        "soft-red": "#fa5757",
+        "grayish-blue": "#9194a1",
+        "dark-blue": "#252b46",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
